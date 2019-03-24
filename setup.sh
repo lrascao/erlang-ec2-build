3b5171726ca36efd340674499dedca03578f7412
Luis Rascao <luis.rascao@miniclip.com>
2019-03-24 21:01:51 +0000

    Add missing docker container image dependencies

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sudo yum install gcc gcc-c++ cpp libmpc-devel mpfr-devel gmp-devel zlib-devel* ncurses-devel -y
+sudo yum install gcc gcc-c++ cpp openssl-devel libmpc-devel mpfr-devel gmp-devel zlib-devel* ncurses-devel tar make automake autoconf patch wget -y
+
+sudo yum-config-manager --enable "Red Hat Enterprise Linux Server 7 Debug (Debug RPMs)"
+yum -y install kernel-debuginfo kernel kernel-devel systemtap systemtap-sdt-devel
