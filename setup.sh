c9b519947bbe572df59cd65a39231faa0a3cfc2b
Luis Rascao <luis.rascao@miniclip.com>
2019-10-22 14:52:13 +0100

    Add missing sudo

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@
 sudo yum install gcc gcc-c++ cpp openssl-devel libmpc-devel mpfr-devel gmp-devel zlib-devel* ncurses-devel tar make automake autoconf patch wget -y
 
 sudo yum-config-manager --enable "Red Hat Enterprise Linux Server 7 Debug (Debug RPMs)"
-yum -y install kernel-debuginfo kernel kernel-devel systemtap systemtap-sdt-devel
+sudo yum -y install kernel-debuginfo kernel kernel-devel systemtap systemtap-sdt-devel
